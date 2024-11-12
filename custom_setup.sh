1ee9e0a08da9c8b8fea0dd68a1c24da8bd71c78b
leo <98397018+comptegithubleo@users.noreply.github.com>
2024-11-12 18:58:09 +0100

    Update custom_setup.sh

--- custom_setup.sh
+++ custom_setup.sh
@@ -1,41 +1,69 @@
 #!/usr/bin/env bash
 
+dpkg_from_url() {
+        TEMP_DEB="$(mktemp)" &&
+        wget -O "$TEMP_DEB" $1 &&
+        sudo dpkg --skip-same-version -i "$TEMP_DEB"
+        rm -f "$TEMP_DEB"
+}
+
+
 while :
 do
-	echo "| ==--== INSTALL ==--==
+        echo "| ==--== INSTALL ==--==
 | PROGRAMS
-|       1) ranger
-|       2) docker-compose
-|       3) all
+|       ranger
+|       docker-compose
+|	codium
+|           - open-source vscode
+|       nvm
+|           - npm version manager - v0.40.1
+|       all-programs
 |
 | MISC
-|       4) cowsay
-|       5) all
+|       cowsay
+|	editor
+|           - choose default editor (nano, vim, ...)
+|       all-misc
 |
 | 'q' or 'quit' to quit
 "
 
-	read -p "Select things to install  > " input
+        read -p "Select things to install  > " input
 
-	if [[ $input = q ]] || [[ $input = quit ]]; then
-		break
-	else
-		case $input in
-        	1)
-        	        yes | apt install ranger
-			printf "\nranger installed\n\n"
-        	        ;;
-        	2)
+        if [[ $input = q ]] || [[ $input = quit ]]; then
+                break
+        else
+                case $input in
+                ranger)
+                        yes | apt install ranger
+                        printf "\nranger installed\n\n"
+                        ;;
+                docker-compose)
                         yes | apt install docker-compose
                         printf "\ndocker-compose installed\n\n"
-			;;
-		*)
+                        ;;
+                codium)
+                        dpkg_from_url https://github.com/VSCodium/vscodium/releases/download/1.95.2.24313/codium_1.95.2.24313_amd64.deb
+                        ;;
+                nvm)
+                        # https://github.com/nvm-sh/nvm?tab=readme-ov-file#installing-and-updating
+                        wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+                        export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+                        [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+                        ;;
+                cowsay)
                         yes | apt install cowsay
-			cp ~/.bashrc ~/.bashrc.$(date +"%d-%m-%Y-%Hh%M")
-			printf "cowsay bjr bon courage\nalias clear='clear; cowsay CLEAR'" >> ~/.bashrc
-			source ~/.bashrc
+                        cp ~/.bashrc ~/.bashrc.$(date +"%d-%m-%Y-%Hh%M")
+                        printf "cowsay bjr bon courage\nalias clear='clear; cowsay CLEAR'" >> ~/.bashrc
+                        source ~/.bashrc
                         printf "\ncowsay installed\n\n"
-			;;
-		esac
-	fi
+                        ;;
+                editor)
+                        update-alternatives --config editor
+                        ;;
+                *)
+                        ;;
+                esac
+        fi
 done
